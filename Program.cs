3f8eb23d119984b67e26a3b4570c3ddc96cccfa0
dhsaifee <dhsaifee@cba.ua.edu>
2020-09-04 01:55:14 -0500

    commit 9/4/2020 2:00 AM

--- Program.cs
+++ Program.cs
@@ -8,6 +8,12 @@ namespace examplerepo
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("I am Danish");
+            AddNumbers(10, 2);
+        }
+
+        static void AddNumbers(int x, int y)
+        {
+            Console.WriteLine("The sum is " + (x + y));
         }
     }
 }
